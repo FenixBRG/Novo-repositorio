ee4b710e083b78a3f30421cba9806ed593eb5ab1
unknown <angelobielkta@outlook.com>
2022-09-12 11:12:57 -0300

    programa para reproduzir o numero desejado

---
+++ programinha.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float n1;
+    printf("Esse eh o programinha do pai, escreva um numero:\n");
+    scanf("%f",&n1);
+
+    printf("\n seu numero eh %.0f",n1);
+    return 0;
+}
